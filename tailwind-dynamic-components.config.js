1f7e7175fa7c021176e181f32fef1748b5504703
Benjamin Preiss <benjaminpreiss@protonmail.com>
2024-01-05 22:12:16 +0100

    Add files list to packagejson

--- tailwind-dynamic-components.config.js
+++
@@ -1,7 +0,0 @@
-export async function defineConfig() {
-    return {
-        debug: false,
-        /* Styles within CompoundStyles have to be registered seperately for their detection */
-        registrations: [],
-    };
-}
